d4e0fa85e2be5a425dfaf5a6a3cba68ff1424f0e
RocketaTemo <rocketatemo@gmail.com>
2020-05-11 17:12:58 +0300

    Feat: Add fillable columns for protecting

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogCategory extends Model
 {
-    //
+    use SoftDeletes; //для deleted_at
+
+    protected $fillable = [ //колонки, которые разрешаем заполнять
+        'title',
+        'alias',
+        'parent_id',
+        'description',
+    ];
 }
